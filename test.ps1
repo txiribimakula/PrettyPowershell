9bba5d302d0b49fb1a5d7e4add85c3644aa2d651
txiribimakula <pablotobalina@gmail.com>
2021-10-12 13:10:07 +0200

    how to wait for keystrokes

--- test.ps1
+++ test.ps1
@@ -2,3 +2,15 @@ Clear-Host
 Write-Host "Whole text is in green" -ForegroundColor White
 Write-Host "Whole text is in red" -ForegroundColor White
 Write-Host "Whole text is in white" -ForegroundColor White
+$PressedKey = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Clear-Host
+Write-Host "Whole text is in green" -ForegroundColor White
+Write-Host "Whole text is in red" -ForegroundColor Green
+Write-Host "Whole text is in white" -ForegroundColor White
+$PressedKey = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Clear-Host
+Write-Host "Whole text is in green" -ForegroundColor White
+Write-Host "Whole text is in red" -ForegroundColor White
+Write-Host "Whole text is in white" -ForegroundColor Green
+$PressedKey = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+$PressedKey.VirtualKeyCode
