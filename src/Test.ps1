24d3c719f0d445596314a4df0433d84ae901385a
txiribimakula <pablotobalina@gmail.com>
2021-10-12 19:24:59 +0200

    options as variable

--- src/Test.ps1
+++ src/Test.ps1
@@ -1,9 +1,12 @@
 Import-Module -Name ".\OptionsSelector.psm1"
 
+$options = @("Option 1", "Option 2", "Option 3", "Option 4")
+
 Write-Host "Select an option:"
-GetSelectedOption @("Option 1", "Option 2", "Option 3", "Option 4")
+$selectedOption = GetSelectedOption $options
+
 
 Write-Host ""
 
 Write-Host "Select options:"
-GetSelectedOptions @("Option 1", "Option 2", "Option 3", "Option 4")
+GetSelectedOptions $options
