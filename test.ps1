02e0516a8d63d8746c50ca17eb60970d10f8b8ac
txiribimakula <pablotobalina@gmail.com>
2021-10-12 13:18:39 +0200

    change color based on line number

--- test.ps1
+++ test.ps1
@@ -3,12 +3,12 @@ $currentLine = 0
 while($pressedKey.VirtualKeyCode -ne 13)
 {
     Clear-Host
-    $pressedKey
+    $currentLine
     Write-Host "Whole text is in green" -ForegroundColor $(If ($currentLine -eq 0) {"Green"} Else {"White"})
-    Write-Host "Whole text is in red" -ForegroundColor White
-    Write-Host "Whole text is in white" -ForegroundColor White
+    Write-Host "Whole text is in red" -ForegroundColor $(If ($currentLine -eq 1) {"Green"} Else {"White"})
+    Write-Host "Whole text is in white" -ForegroundColor $(If ($currentLine -eq 2) {"Green"} Else {"White"})
     $pressedKey = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-    if($pressedKey -eq 40) {
+    if($pressedKey.VirtualKeyCode -eq 40) {
         $currentLine = $currentLine + 1
     }
 }
