c6023e3bda669e5188aa28df9a545fb135e8dc67
txiribimakula <pablotobalina@gmail.com>
2021-10-12 17:02:04 +0200

    change options texts

--- test.ps1
+++ test.ps1
@@ -4,9 +4,9 @@ $selectedLines = 0
 while($pressedKey.VirtualKeyCode -ne 13)
 {
     Clear-Host
-    Write-Host $(If ($selectedLines -band 1) {"[x]"} Else {"[ ]"}) "Whole text is in green" -ForegroundColor $(If ($currentLine -eq 0) {"Green"} Else {"White"})
-    Write-Host $(If ($selectedLines -band 2) {"[x]"} Else {"[ ]"}) "Whole text is in red" -ForegroundColor $(If ($currentLine -eq 1) {"Green"} Else {"White"})
-    Write-Host $(If ($selectedLines -band 4) {"[x]"} Else {"[ ]"}) "Whole text is in white" -ForegroundColor $(If ($currentLine -eq 2) {"Green"} Else {"White"})
+    Write-Host $(If ($selectedLines -band 1) {"[x]"} Else {"[ ]"}) "Option 1" -ForegroundColor $(If ($currentLine -eq 0) {"Green"} Else {"White"})
+    Write-Host $(If ($selectedLines -band 2) {"[x]"} Else {"[ ]"}) "Option 2" -ForegroundColor $(If ($currentLine -eq 1) {"Green"} Else {"White"})
+    Write-Host $(If ($selectedLines -band 4) {"[x]"} Else {"[ ]"}) "Option 3" -ForegroundColor $(If ($currentLine -eq 2) {"Green"} Else {"White"})
     $pressedKey = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     if($pressedKey.VirtualKeyCode -eq 40) {
         $currentLine = $currentLine + 1
