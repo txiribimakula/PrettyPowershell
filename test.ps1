b87ec906d3f67abf5ccd32a0af72613f9a6ada6c
txiribimakula <pablotobalina@gmail.com>
2021-10-12 13:17:26 +0200

    increment line number on down key

--- test.ps1
+++ test.ps1
@@ -3,8 +3,12 @@ $currentLine = 0
 while($pressedKey.VirtualKeyCode -ne 13)
 {
     Clear-Host
+    $pressedKey
     Write-Host "Whole text is in green" -ForegroundColor $(If ($currentLine -eq 0) {"Green"} Else {"White"})
     Write-Host "Whole text is in red" -ForegroundColor White
     Write-Host "Whole text is in white" -ForegroundColor White
     $pressedKey = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    if($pressedKey -eq 40) {
+        $currentLine = $currentLine + 1
+    }
 }
