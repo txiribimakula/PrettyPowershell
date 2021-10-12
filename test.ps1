43f2fb036c9272ed38c7d053617e0beae1cba39f
txiribimakula <pablotobalina@gmail.com>
2021-10-12 13:21:43 +0200

    do not show current line

--- test.ps1
+++ test.ps1
@@ -3,7 +3,6 @@ $currentLine = 0
 while($pressedKey.VirtualKeyCode -ne 13)
 {
     Clear-Host
-    $currentLine
     Write-Host "Whole text is in green" -ForegroundColor $(If ($currentLine -eq 0) {"Green"} Else {"White"})
     Write-Host "Whole text is in red" -ForegroundColor $(If ($currentLine -eq 1) {"Green"} Else {"White"})
     Write-Host "Whole text is in white" -ForegroundColor $(If ($currentLine -eq 2) {"Green"} Else {"White"})
