bc678557c62fc5931fbab3a943488bae056d4a2e
txiribimakula <pablotobalina@gmail.com>
2021-10-12 13:16:20 +0200

    color depending on variable

--- test.ps1
+++ test.ps1
@@ -1,7 +1,9 @@
+$currentLine = 0
+
 while($pressedKey.VirtualKeyCode -ne 13)
 {
     Clear-Host
-    Write-Host "Whole text is in green" -ForegroundColor Green
+    Write-Host "Whole text is in green" -ForegroundColor $(If ($currentLine -eq 0) {"Green"} Else {"White"})
     Write-Host "Whole text is in red" -ForegroundColor White
     Write-Host "Whole text is in white" -ForegroundColor White
     $pressedKey = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
