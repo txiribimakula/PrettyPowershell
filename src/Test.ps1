878bf22c65795c0f0aede2d90a698df097c1c59d
txiribimakula <pablotobalina@gmail.com>
2021-10-12 19:29:38 +0200

    show selections

--- src/Test.ps1
+++ src/Test.ps1
@@ -9,4 +9,9 @@ Write-Host "Selection:" $options[$selectedOption]
 Write-Host ""
 
 Write-Host "Select options:"
-GetSelectedOptions $options
+$selectedOptions = GetSelectedOptions $options
+for ($i = 0; $i -lt $options.Count; $i++) {
+    if($selectedOptions -band [math]::Pow(2, $i)) {
+        Write-Host "Selection:" $options[$i]
+    }
+}
