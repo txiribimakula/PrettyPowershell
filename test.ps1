dc3087030e6f1b79a01e7da270b761c1bd2dae43
txiribimakula <pablotobalina@gmail.com>
2021-10-12 13:23:41 +0200

    change line content based on variable

--- test.ps1
+++ test.ps1
@@ -3,7 +3,7 @@ $currentLine = 0
 while($pressedKey.VirtualKeyCode -ne 13)
 {
     Clear-Host
-    Write-Host "Whole text is in green" -ForegroundColor $(If ($currentLine -eq 0) {"Green"} Else {"White"})
+    Write-Host $(If ($currentLine -ne 0) {"[x]"} Else {"[ ]"}) "Whole text is in green" -ForegroundColor $(If ($currentLine -eq 0) {"Green"} Else {"White"})
     Write-Host "Whole text is in red" -ForegroundColor $(If ($currentLine -eq 1) {"Green"} Else {"White"})
     Write-Host "Whole text is in white" -ForegroundColor $(If ($currentLine -eq 2) {"Green"} Else {"White"})
     $pressedKey = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
