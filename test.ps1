32b7481ab98136247ebf14083935b71c6d9740e3
txiribimakula <pablotobalina@gmail.com>
2021-10-12 17:27:57 +0200

    change variable name

--- test.ps1
+++ test.ps1
@@ -1,24 +1,24 @@
-$currentLine = 0
+$selectedLine = 0
 $selectedLines = 0
 
 while($pressedKey.VirtualKeyCode -ne 13)
 {
-    Write-Host $(If ($selectedLines -band 1) {"[x]"} Else {"[ ]"}) "Option 1" -ForegroundColor $(If ($currentLine -eq 0) {"Green"} Else {"White"})
-    Write-Host $(If ($selectedLines -band 2) {"[x]"} Else {"[ ]"}) "Option 2" -ForegroundColor $(If ($currentLine -eq 1) {"Green"} Else {"White"})
-    Write-Host $(If ($selectedLines -band 4) {"[x]"} Else {"[ ]"}) "Option 3" -ForegroundColor $(If ($currentLine -eq 2) {"Green"} Else {"White"})
+    Write-Host $(If ($selectedLines -band 1) {"[x]"} Else {"[ ]"}) "Option 1" -ForegroundColor $(If ($selectedLine -eq 0) {"Green"} Else {"White"})
+    Write-Host $(If ($selectedLines -band 2) {"[x]"} Else {"[ ]"}) "Option 2" -ForegroundColor $(If ($selectedLine -eq 1) {"Green"} Else {"White"})
+    Write-Host $(If ($selectedLines -band 4) {"[x]"} Else {"[ ]"}) "Option 3" -ForegroundColor $(If ($selectedLine -eq 2) {"Green"} Else {"White"})
     $pressedKey = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     if($pressedKey.VirtualKeyCode -eq 40) {
-        $currentLine = $currentLine + 1
-        if($currentLine -eq 3) {
-            $currentLine = 0
+        $selectedLine = $selectedLine + 1
+        if($selectedLine -eq 3) {
+            $selectedLine = 0
         }
     } elseif($pressedKey.VirtualKeyCode -eq 38) {
-        $currentLine = $currentLine - 1
-        if($currentLine -eq -1) {
-            $currentLine = 2
+        $selectedLine = $selectedLine - 1
+        if($selectedLine -eq -1) {
+            $selectedLine = 2
         }
     } elseif($pressedKey.VirtualKeyCode -eq 32) {
-        $selectedLines = $selectedLines -bxor [math]::Pow(2, $currentLine)
+        $selectedLines = $selectedLines -bxor [math]::Pow(2, $selectedLine)
     }
 
     $currentCursorPosition  = $Host.UI.RawUI.CursorPosition.Y
