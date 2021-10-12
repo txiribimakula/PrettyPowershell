0c544d8720b0a51db151b75022a5f89e33b36786
txiribimakula <pablotobalina@gmail.com>
2021-10-12 13:44:53 +0200

    use bitwise to store selection

--- test.ps1
+++ test.ps1
@@ -4,9 +4,9 @@ $selectedLines = 0
 while($pressedKey.VirtualKeyCode -ne 13)
 {
     Clear-Host
-    Write-Host $(If ($selectedLines -eq 1) {"[x]"} Else {"[ ]"}) "Whole text is in green" -ForegroundColor $(If ($currentLine -eq 0) {"Green"} Else {"White"})
-    Write-Host "Whole text is in red" -ForegroundColor $(If ($currentLine -eq 1) {"Green"} Else {"White"})
-    Write-Host "Whole text is in white" -ForegroundColor $(If ($currentLine -eq 2) {"Green"} Else {"White"})
+    Write-Host $(If ($selectedLines -band 1) {"[x]"} Else {"[ ]"}) "Whole text is in green" -ForegroundColor $(If ($currentLine -eq 0) {"Green"} Else {"White"})
+    Write-Host $(If ($selectedLines -band 2) {"[x]"} Else {"[ ]"}) "Whole text is in red" -ForegroundColor $(If ($currentLine -eq 1) {"Green"} Else {"White"})
+    Write-Host $(If ($selectedLines -band 4) {"[x]"} Else {"[ ]"}) "Whole text is in white" -ForegroundColor $(If ($currentLine -eq 2) {"Green"} Else {"White"})
     $pressedKey = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     if($pressedKey.VirtualKeyCode -eq 40) {
         $currentLine = $currentLine + 1
@@ -19,6 +19,6 @@ while($pressedKey.VirtualKeyCode -ne 13)
             $currentLine = 2
         }
     } elseif($pressedKey.VirtualKeyCode -eq 32) {
-        $selectedLines = $selectedLines + 1 + $currentLine
+        $selectedLines = $selectedLines -bxor [math]::Pow(2, $currentLine)
     }
 }
