133428d2305c2391de5c40633107b6c2a91b5cd4
txiribimakula <pablotobalina@gmail.com>
2021-10-12 19:23:17 +0200

    additional texts

--- src/Test.ps1
+++ src/Test.ps1
@@ -1,4 +1,9 @@
 Import-Module -Name ".\OptionsSelector.psm1"
 
+Write-Host "Select an option:"
 GetSelectedOption @("Option 1", "Option 2", "Option 3", "Option 4")
+
+Write-Host ""
+
+Write-Host "Select options:"
 GetSelectedOptions @("Option 1", "Option 2", "Option 3", "Option 4")
