46c84cbf64d531b88662b8a71cf02b420dec0616
txiribimakula <pablotobalina@gmail.com>
2021-10-12 19:26:18 +0200

    show selection

--- src/Test.ps1
+++ src/Test.ps1
@@ -4,7 +4,7 @@ $options = @("Option 1", "Option 2", "Option 3", "Option 4")
 
 Write-Host "Select an option:"
 $selectedOption = GetSelectedOption $options
-
+Write-Host "Selection:" $options[$selectedOption]
 
 Write-Host ""
 
